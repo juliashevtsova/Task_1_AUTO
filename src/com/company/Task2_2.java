af83815e6324f7ecb6d01b5f9a4a8df5d68da1cb
Julia shevtsova <juliashevtsova96@gmail.com>
2021-12-08 15:13:41 -0800

    output of odd numbers by "for"

---
+++ src/com/company/Task2_2.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task2_2 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input n:");
+        int n = in.nextInt();
+        int answer = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            answer = answer * i;
+        }
+        System.out.println(answer);
+    }
+}
+
+
+
